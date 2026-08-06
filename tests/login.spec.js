251a53becbb5ffa995df506c8e0dcac93889ad20
Maksim Ometcinskii <applause1max@gmail.com>
2026-08-06 10:45:18 -0400

    Add JS Playwright login tests (TC-001 to TC-004)

---
+++ tests/login.spec.js
@@ -0,0 +1,41 @@
+const { test, expect } = require ('@playwright/test');
+
+const STANDARD_USER = 'standard_user';
+const LOCKED_OUT_USER = 'locked_out_user';
+const PASSWORD = 'secret_sauce';
+
+test('TC-001: Valid login redirects to inventory page', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.getByRole('textbox', { name: 'Username' }).fill(STANDARD_USER);
+    await page.getByRole('textbox', { name: 'Password' }).fill(PASSWORD);
+    await page.getByRole('button', { name: 'Login' }).click();
+    await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
+});
+
+
+test('TC-002: Locked out login shows corresponding error message', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.getByRole('textbox', {name: 'Username'}).fill(LOCKED_OUT_USER);
+    await page.getByRole('textbox', {name: 'Password'}).fill(PASSWORD);
+    await page.getByRole('button', {name: 'Login'}).click();
+    await expect(page).toHaveURL('https://www.saucedemo.com/');
+    await expect(page.getByText('locked out')).toBeVisible();
+});
+
+test('TC-003: Invalid password shows corresponding error message', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.getByRole('textbox', {name: 'Username'}).fill(STANDARD_USER);
+    await page.getByRole('textbox', {name: 'Password'}).fill('Wrong_password');
+    await page.getByRole('button', {name: 'Login'}).click();
+    await expect(page).toHaveURL('https://www.saucedemo.com/');
+    await expect(page.getByText('Username and password do not match any user in this service')).toBeVisible();
+});
+
+test('TC-004: Empty username show corresponding error message', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    await page.getByRole('textbox', {name: 'Password'}).fill(PASSWORD);
+    await page.getByRole('button', {name: 'Login'}).click();
+    await expect(page).toHaveURL('https://www.saucedemo.com/');
+    await expect(page.getByText('Username is required')).toBeVisible();
+});
+
