0b33e1d2da3ac077c619d8512478a9297eb6763a
Maksim Ometcinskii <applause1max@gmail.com>
2026-08-11 18:50:12 -0400

    Added TC010 and TC011, TC001 with known issue

--- tests/sorting.spec.js
+++ tests/sorting.spec.js
@@ -1,3 +1,4 @@
+const { log } = require('node:console');
 const {test, expect} = require('./fixtures');
 
 /** @param {{ loggedInPage: import('@playwright/test').Page }} params */
@@ -21,6 +22,39 @@ test('TC-009: Verify sorting from lowest price', async ({ loggedInPage }) => {
     await loggedInPage.locator('[data-test="product-sort-container"]').selectOption('lohi');
     const pricing = await loggedInPage.locator('[data-test="inventory-item-price"]').allTextContents();
     
+    const prices = [];
+    for (const price of pricing) {
+        const cleanPrice = price.replace('$', '');
+        const number = parseFloat(cleanPrice);
+        prices.push(number);
+    }
+
+    expect(prices).toEqual([...prices].sort((a, b) => a - b));
+});
+
+/** @param {{ loggedInPage: import('@playwright/test').Page }} params */
+test('TC-010: Verify sorting from highest price', async ({ loggedInPage }) => {
+    await loggedInPage.locator('[data-test="product-sort-container"]').selectOption('hilo');
+    const pricing = await loggedInPage.locator('[data-test="inventory-item-price"]').allTextContents();
+
+    const prices = [];
+    for (const price of pricing) {
+        const cleanPrice = price.replace('$', '');
+        const number = parseFloat(cleanPrice);
+        prices.push(number);
+    }
+    
+    expect(prices).toEqual([...prices].sort((a, b) => b - a));
+});
+
+/** @param {{ loggedInPage: import('@playwright/test').Page }} params */
+test('TC-011: Verify sorting applied when navigating back from product page', async ({ loggedInPage }) => {
+    test.fail();
+    await loggedInPage.locator('[data-test="product-sort-container"]').selectOption('lohi');
+    await loggedInPage.locator('[data-test="inventory-item-name"]').first().click();
+    await loggedInPage.getByRole('button', {name: "Back to Products"}).click();
+
+    const pricing = await loggedInPage.locator('[data-test="inventory-item-price"]').allTextContents();
     const prices = [];
     for (const price of pricing) {
         const cleanPrice = price.replace('$', '');
