7e6f0ff611833d18807351be8d97e8d105d56718
Maksim Ometcinskii <applause1max@gmail.com>
2026-08-24 21:24:56 -0400

    Add LoginPage POM, refactor TC-001 to TC-006 to use it

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,55 +1,55 @@
 import { test, expect } from '@playwright/test';
+import { LoginPage } from '../pages/LoginPage';
+import { log } from 'node:console';
 
 const STANDARD_USER = 'standard_user';
 const LOCKED_OUT_USER = 'locked_out_user';
 const PASSWORD = 'secret_sauce';
 
 test('TC-001: Valid login redirects to inventory page', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com/');
-    await page.getByRole('textbox', { name: 'Username' }).fill(STANDARD_USER);
-    await page.getByRole('textbox', { name: 'Password' }).fill(PASSWORD);
-    await page.getByRole('button', { name: 'Login' }).click();
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login(STANDARD_USER, PASSWORD);
     await expect(page).toHaveURL('https://www.saucedemo.com/inventory.html');
 });
 
 
 test('TC-002: Locked out login shows corresponding error message', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com/');
-    await page.getByRole('textbox', {name: 'Username'}).fill(LOCKED_OUT_USER);
-    await page.getByRole('textbox', {name: 'Password'}).fill(PASSWORD);
-    await page.getByRole('button', {name: 'Login'}).click();
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login(LOCKED_OUT_USER, PASSWORD);
     await expect(page).toHaveURL('https://www.saucedemo.com/');
     await expect(page.getByText('locked out')).toBeVisible();
 });
 
 test('TC-003: Invalid password shows corresponding error message', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com/');
-    await page.getByRole('textbox', {name: 'Username'}).fill(STANDARD_USER);
-    await page.getByRole('textbox', {name: 'Password'}).fill('Wrong_password');
-    await page.getByRole('button', {name: 'Login'}).click();
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login(STANDARD_USER,'Wrong_password');
     await expect(page).toHaveURL('https://www.saucedemo.com/');
     await expect(page.getByText('Username and password do not match any user in this service')).toBeVisible();
 });
 
 test('TC-004: Empty username show corresponding error message', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com/');
-    await page.getByRole('textbox', {name: 'Password'}).fill(PASSWORD);
-    await page.getByRole('button', {name: 'Login'}).click();
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login("", PASSWORD);
     await expect(page).toHaveURL('https://www.saucedemo.com/');
     await expect(page.getByText('Username is required')).toBeVisible();
 });
 
 test('TC-005: Invalid login with empty password', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com/');
-    await page.getByRole('textbox', {name: 'Username'}).fill(STANDARD_USER);
-    await page.getByRole('button', {name: 'Login'}).click();
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login(STANDARD_USER, "");
     await expect(page).toHaveURL('https://www.saucedemo.com/');
     await expect(page.getByText('Password is required')).toBeVisible();
 });
 
 test('TC-006: Invalid login with empty fields', async ({ page }) => {
-    await page.goto('https://www.saucedemo.com/');
-    await page.getByRole('button', {name: "Login"}).click();
+    const loginPage = new LoginPage(page);
+    await loginPage.goto();
+    await loginPage.login("","");
     await expect(page.getByText('Username is required')).toBeVisible();
     await expect(page).toHaveURL('https://www.saucedemo.com/');
 });
